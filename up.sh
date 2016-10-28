c9e8fa8f5978115b23113d7fbfaef6383f3f75f5
Ryan Keleti <ryankeleti@users.noreply.github.com>
2016-10-28 12:47:09 -0400

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+#==================================#
+# > this should only be used when  #
+#   making backups to repo         #
+# > because of this, commits will  #
+#   not have specifics             #
+# > if you us this, make sure you  #
+#   know what it does and are      #
+#   running from a Unix system     #
+#   ie) Linux, OSX, etc.           #
+#       NOT WINDOWS                #
+#     DO NOT MODIFY THIS CODE      #
+# =================================#
+
 function gitEX () {
 local EX="$?"
-
+# checks escape seq and returns error
+# message if esc seq is not 0
   if [ $EX != 0 ]; then
     echo ":: error"
   else
@@ -10,10 +24,20 @@ local EX="$?"
   fi
 }
 
+# $HOME is an env var that holds your
+# home dir.
+# example, if I am user "ryan_u",
+# then my home dir is
+#   /home/ryan_u/
+# PUT YOUR bpr REPO IN YOUR HOME DIR
+# IF NOT, CHANGE THIS LINE
 cd $HOME/bpr
 
+# includes all files in current dir
+# change this to suit your needs
 files="*"
 
+# verifies
 echo -n "continue [y/n] "
 read verify
 
