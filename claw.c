9a4930648bd5cefa39ee04f0eab577478ab31b28
Ryan Keleti <ryankeleti@users.noreply.github.com>
2017-03-03 14:59:24 -0500

    Update claw.c

--- claw.c
+++ claw.c
@@ -1,33 +1,29 @@
 /* Function to open/close claws
- * Created by Harrison Jumper */ 
-  
-#include <kipr/botball.h>  
- 
-//claw closed position = 
-//claw open position = 
- 
- 
-void claw_change(op, wp);//op is the original position and wp is the wanted position
-  
-int servoport1 = 0;//change to correct port  
-int speed = 2;//change to speed you want the claw to open/close at 
- 
- 
-int main()  
-{  
-enable_servos();  
-claw_change;  
-disable_servos();  
-return 0;  
-}  
-  
-void claw_change(op, wp)
-{  
-while(op > wp) //change to < if positions need it 
-{  
-op = op - speed;//change to - if positions need it 
-set_servo_position(servoport1, op);  
-printf("opening or closing claw/");//to debug issues with the code 
-msleep(100);
-}  
-}   
+ * Created by Harrison Jumper
+ *            and Ryan Keleti */ 
+#include <kipr/botball.h>
+
+/* please, for the love of God, fix these enums
+ * to their appropriate values */
+enum {
+  CLAW_CLOSE=0,
+  CLAW_OPEN=180
+};
+
+void claw(int op, int wp);  
+int servoport1 = 0; /* change to correct port */
+int speed = 2;      /* change to speed you want the claw to open/close at */
+
+void claw(int orig, int new)
+{
+  if (new<CLAW_CLOSE || new>CLAW_OPEN) {
+    printf("invalid claw position\n");
+    return;
+  }
+  while (orig > new) { /* change to < if positions need it <--- ... */ 
+    orig -= speed; /*change to - if positions need it */
+    set_servo_position(servoport1, orig); 
+    printf("opening or closing claw\n");//to debug issues with the code
+    msleep(100);
+  }
+}
